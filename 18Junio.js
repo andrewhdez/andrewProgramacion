0e7bf0320146c0deb92ddb11304a3ecc1ac7f1c6
David Hde <davyd2h@gmail.com>
2025-06-18 09:41:59 -0500

    ejercicios del 18 de junio

---
+++ 18Junio.js
@@ -0,0 +1,57 @@
+// Ejercicio 1
+// Crea un programa que imprima los números del 1 al 10 usando un bucle for
+
+
+
+// Ejercicio 2
+// Crea una función que reciba un número y retorne "par" si es par o "impar" si es impar
+
+
+
+// Ejercicio 3
+// Crea un programa que imprima la tabla de multiplicar del 5 (del 1 al 10)
+
+
+
+// Ejercicio 4
+// Crea una función que reciba una edad y diga si puede votar (mayor o igual a 18)
+
+
+
+// Ejercicio 5
+// Crea un programa que sume todos los números del 1 al 100
+
+
+
+// Ejercicio 6
+// Crea una función que reciba un nombre y salude con "Hola, [nombre]!"
+
+
+
+// Ejercicio 7
+// Crea un programa que imprima todos los números pares del 1 al 50
+
+
+
+// Ejercicio 8
+// Crea una función que reciba una nota (0 a 10) y devuelva el mensaje: "Aprobado" si es >= 6, "Reprobado" si es menor
+
+
+
+// Ejercicio 9
+// Crea un programa que imprima los números del 10 al 1 en orden descendente
+
+
+
+// Ejercicio 10
+// Crea una función que reciba un número y devuelva su factorial
+
+
+
+// Ejercicio 11
+// Crea un programa que muestre los primeros 10 números de la secuencia de Fibonacci
+
+
+
+// Ejercicio 12
+// Crea una función que reciba dos números y devuelva cuál es mayor
