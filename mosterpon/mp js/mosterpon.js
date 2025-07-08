fa7bf5b032f7c9206ad3d15d6565f9fc345c7e3c
Andres Hernandez <herdzandre@gmail.com>
2025-07-07 21:47:16 -0500

    mosterpon

--- mosterpon/mp js/mosterpon.js
+++ mosterpon/mp js/mosterpon.js
@@ -1,6 +1,16 @@
+let ataqueMostruo
+let ataqueEnemigo
+
 function iniciarJuego() {
     let botonMostruoJugador = document.getElementById('boton-mostruo')
     botonMostruoJugador.addEventListener('click', seleccionarMostruoJugador)
+
+    let botonCerebro = document.getElementById('boton-cerebro')
+    botonCerebro.addEventListener('click', ataqueCerebro)
+    let botonSangre = document.getElementById('boton-sangre')
+    botonSangre.addEventListener('click', ataqueSangre)
+    let botonCalabazas = document.getElementById('boton-calabazas')
+    botonCalabazas.addEventListener('click', ataqueCalabazas)
 }
 
 function seleccionarMostruoJugador() {
@@ -23,16 +33,67 @@ function seleccionarMostruoJugador() {
 }
 
 function seleccionarMostruoEnemigo() {
-    let ataqueAleatorio = aleatorio(1,3)
+    let mostruoAleatorio = aleatorio(1,3)
     let spanMostruoEnemigo = document.getElementById('mostruo-enemigo')
 
-    if (ataqueAleatorio == 1) {
+    if (mostruoAleatorio == 1) {
         spanMostruoEnemigo.innerHTML = 'Zombitre'
-    } else if (ataqueAleatorio == 2) {
+    } else if (mostruoAleatorio == 2) {
         spanMostruoEnemigo.innerHTML = 'Draculin'
     } else {
         spanMostruoEnemigo.innerHTML = 'Franquestine'
     }
+}
+
+function ataqueCerebro() {
+    ataqueMostruo = 'CEREBRO'
+    ataqueAleatorioEnemigo()
+}
+function ataqueSangre() {
+    ataqueMostruo = 'SANGRE'
+    ataqueAleatorioEnemigo()
+}
+function ataqueCalabazas() {
+    ataqueMostruo = 'CALABAZAS'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+
+    if (ataqueAleatorio == 1) {
+        ataqueEnemigo = 'CEREBRO'
+    } else if (ataqueAleatorio == 2) {
+        ataqueEnemigo = 'SANGRE'
+    } else {
+        ataqueEnemigo = 'CALABAZAS'
+    }
+
+    combate()
+}
+
+function combate() {
+        if(ataqueEnemigo == ataqueMostruo) {
+            crearMensaje("EMPATE")
+        } else if(ataqueMostruo == 'CEREBRO' && ataqueEnemigo == 'CALABAZAS') {
+            crearMensaje("GANASTE")
+        } else if(ataqueMostruo == 'SANGRE' && ataqueEnemigo == 'CEREBRO') {
+            crearMensaje("GANASTE")
+        } else if(ataqueMostruo == 'CALABAZAS' && ataqueEnemigo == 'SANGRE') {
+            crearMensaje("GANASTE")
+        } else {
+            crearMensaje("PERDISTE")
+        }
+    }
+
+function crearMensaje(resultado) {
+    let sectionMensajes = document.getElementById('mensajes')
+
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = 'tu mascota ataco con ' + ataqueMostruo + ', la mascota del enemigo ataco con '
+    + ataqueEnemigo + '- ' + resultado
+
+    sectionMensajes.appendChild(parrafo)
 }
 
 function aleatorio(min,max) {
